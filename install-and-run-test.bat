940ba712f6f5fd15c759f567e3754274a534d2f9
Andrey Komarov (k0m@) <andrey@komarov.pw>
2021-03-01 08:29:55 +0300

    Both linux/windows batch runner

--- install-and-run-test.bat
+++ install-and-run-test.bat
@@ -1,5 +1,18 @@
+:<<"::CMDLITERAL"
+@ECHO OFF
+GOTO :CMDSCRIPT
+::CMDLITERAL
+#linux commands
 cd ./core
 mvn install
 cd ./../runner
 mvn test
+cd ..
+
+:CMDSCRIPT
+rem Windows commands:
+cd ./core
+cmd /c mvn install
+cd ./../runner
+cmd /c mvn test
 cd ..
